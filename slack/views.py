96b4e0a55f82e55a1ba35884729f68b140a58ac9
Zach Tibbitts <zachtib@gmail.com>
2020-01-15 17:14:48 -0500

    debugging info

--- slack/views.py
+++ slack/views.py
@@ -7,13 +7,18 @@ from django.http import HttpResponse
 
 def verify_slack_request(request):
     request_body = request.body()
+    print(request_body)
     slack_request_timestamp = request.headers['X-Slack-Request-Timestamp']
+    print(slack_request_timestamp)
     slack_signature = request.headers['X-Slack-Signature']
+    print(slack_signature)
 
     basestring = f"v0:{slack_request_timestamp}:{request_body}".encode('utf-8')
+    print(basestring)
     slack_signing_secret = bytes(settings.SLACK_SIGNING_SECRET, 'utf-8')
 
     signature = 'v0=' + hmac.new(slack_signing_secret, basestring, hashlib.sha256).hexdigest()
+    print(signature)
 
     if hmac.compare_digest(signature, slack_signature):
         return True
