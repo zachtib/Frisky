c10f13378aecd48cd8ca5877078fe61c403db342
Zach Tibbitts <zachtib@gmail.com>
2020-01-15 18:28:30 -0500

    cleanup verification

--- slack/views.py
+++ slack/views.py
@@ -11,33 +11,26 @@ logger = logging.getLogger(__name__)
 
 
 def verify_slack_request(request):
-    if request.method == 'POST':
-        form_data = json.loads(request.body.decode())
-        request_body = '&'.join([f'{key}={value}' for key, value in form_data.items()])
-        logger.info(request_body)
-        slack_request_timestamp = request.headers['X-Slack-Request-Timestamp']
-        logger.info(slack_request_timestamp)
-        slack_signature = request.headers['X-Slack-Signature']
-        logger.info(slack_signature)
+    slack_request_timestamp = request.headers['X-Slack-Request-Timestamp']
+    slack_signature = request.headers['X-Slack-Signature']
 
-        basestring = f"v0:{slack_request_timestamp}:{request_body}".encode('utf-8')
-        logger.info(basestring)
-        slack_signing_secret = bytes(settings.SLACK_SIGNING_SECRET, 'utf-8')
+    form_data = json.loads(request.body.decode())
+    request_body = '&'.join([f'{key}={value}' for key, value in form_data.items()])
 
-        signature = 'v0=' + hmac.new(slack_signing_secret, basestring, hashlib.sha256).hexdigest()
-        logger.info(signature)
+    basestring = f"v0:{slack_request_timestamp}:{request_body}".encode('utf-8')
+    slack_signing_secret = bytes(settings.SLACK_SIGNING_SECRET, 'utf-8')
+    signature = 'v0=' + hmac.new(slack_signing_secret, basestring, hashlib.sha256).hexdigest()
 
-        if hmac.compare_digest(signature, slack_signature):
-            return True
-        else:
-            return False
+    if hmac.compare_digest(signature, slack_signature):
+        return True
+    else:
+        return False
 
 
 @csrf_exempt
 def event(request):
-    form_data = json.loads(request.body.decode())
-    logger.info(f'form_data: {form_data}')
     if verify_slack_request(request):
+        form_data = json.loads(request.body.decode())
         if form_data['type'] == 'url_verification':
             return HttpResponse(form_data['challenge'])
 
