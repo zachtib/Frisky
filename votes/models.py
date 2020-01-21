2b58e226a4d874a83f69955d10dbbf02450963d0
Zach Tibbitts <zachtib@gmail.com>
2020-01-20 19:59:59 -0500

    bugfix for reactions

--- votes/models.py
+++ votes/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Vote(models.Model):
     label = models.CharField(max_length=200, db_index=True, unique=True)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Votes for {self.label}'
