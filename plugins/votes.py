52d35e0602488d770266bd7d1a5d66955564ac7b
Zach Tibbitts <zachtib@gmail.com>
2020-01-20 20:02:44 -0500

    don't upvote yourself

--- plugins/votes.py
+++ plugins/votes.py
@@ -3,13 +3,18 @@ from votes.queries import get_votes_record, upvote, downvote
 
 def handle_reaction(reaction, reacting_user, commenting_user, comment, added):
     if reaction == 'upvote':
-        if added:
+        if reacting_user == commenting_user and added:
+            return f'Nice try, {reacting_user}, you can\'t upvote yourself'
+        elif added:
             record = upvote(commenting_user)
             return f'{reacting_user} upvoted {commenting_user}! {commenting_user} has {record.votes} friskypoints.'
         else:
             downvote(commenting_user)
     elif reaction == 'downvote':
-        if added:
+        if reacting_user == commenting_user and added:
+            record = downvote(commenting_user)
+            return f'You CAN however, downvote yourself. You have {record.votes} friskypoints'
+        elif added:
             record = downvote(commenting_user)
             return f'{reacting_user} downvoted {commenting_user}! {commenting_user} has {record.votes} friskypoints.'
         else:
