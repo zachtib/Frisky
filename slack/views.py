078754f37f5810845f948ec4e63e48b72974c3db
Zach Tibbitts <zachtib@gmail.com>
2020-02-02 18:14:21 -0500

    delay event handling

--- slack/views.py
+++ slack/views.py
@@ -8,13 +8,8 @@ from django.http import HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from frisky.bot import handle_message, handle_reaction
-from frisky.http import FriskyResponse
-from slack.api.client import SlackApiClient
-from slack.api.models import Event, ReactionAdded, MessageSent
-
 logger = logging.getLogger(__name__)
-slack_api_client = SlackApiClient(settings.SLACK_ACCESS_TOKEN)
+from slack.tasks import process_event
 
 
 class SlackEvent(View):
@@ -31,7 +26,8 @@ class SlackEvent(View):
         elif 'X-Slack-Retry-Num' in request.headers:
             return HttpResponse(status=200)
         elif form_data['type'] == 'event_callback':
-            return FriskyResponse(lambda: self.process_event(form_data))
+            process_event.delay(form_data)
+            return HttpResponse(status=200)
         else:
             return HttpResponse(status=404)
 
@@ -50,40 +46,3 @@ class SlackEvent(View):
             return True
         else:
             return False
-
-    def process_event(self, form_data):
-        try:
-            event_wrapper = Event.from_dict(form_data)
-            event = event_wrapper.get_event()
-            team = slack_api_client.get_workspace(form_data['team_id'])
-            if isinstance(event, ReactionAdded):
-                user = slack_api_client.get_user(event.user)
-                channel = slack_api_client.get_channel(event.item.channel)
-                item_user = slack_api_client.get_user(event.item_user)
-                added = event.type == 'reaction_added'
-                message = slack_api_client.get_message(channel, event.item.ts)
-
-                handle_reaction(
-                    event.reaction,
-                    user.name,
-                    item_user.name,
-                    message.text,
-                    added,
-                    lambda reply: slack_api_client.post_message(channel, reply)
-                )
-            elif isinstance(event, MessageSent):
-                user = slack_api_client.get_user(event.user)
-                channel = slack_api_client.get_channel(event.channel)
-                if channel.name != 'frisky-logs':
-                    if event.text.endswith('!log'):
-                        slack_api_client.emergency_log(event)
-                        event.text = event.text[:-4].rstrip()
-
-                    handle_message(
-                        channel.name,
-                        user.name,
-                        event.text,
-                        lambda reply: slack_api_client.post_message(channel, reply)
-                    )
-        except Exception as e:
-            print(e)
