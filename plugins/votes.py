13d51487e3a06ff524a85241cf27186c1030d609
Zach Tibbitts <zachtib@gmail.com>
2020-02-11 22:49:33 -0500

    [plugin] Improve Votes (#12)
    * Update votes.py
    
    * Oop
    
    * Votes plugin refactor

--- plugins/votes.py
+++ plugins/votes.py
@@ -17,30 +17,54 @@ class VotesPlugin(FriskyPlugin):
 
     @classmethod
     def register_commands(cls) -> Tuple:
-        return 'votes',
+        return 'votes', 'upvote', 'downvote', '++', '--'
+
+    @staticmethod
+    def __format_score(count):
+        if count == 1:
+            return '1 friskypoint'
+        else:
+            return f'{count} friskypoints'
+
+    def __do_upvote(self, user, thing, silent=False) -> Optional[str]:
+        if user == thing:
+            if not silent:
+                return f'Nice try, {user}, you can\'t upvote yourself'
+        else:
+            record = upvote(thing)
+            if not silent:
+                return f'{user} upvoted {thing}! {thing} has {self.__format_score(record.votes)}'
+
+    def __do_downvote(self, user, thing, silent=False) -> Optional[str]:
+        if user == thing:
+            record = downvote(thing)
+            if not silent:
+                return f'You CAN however, downvote yourself. You have {self.__format_score(record.votes)}. Bwahaha.'
+        else:
+            record = downvote(thing)
+            if not silent:
+                return f'{user} downvoted {thing}! {thing} has {self.__format_score(record.votes)}'
 
     def handle_message(self, message: MessageEvent) -> Optional[str]:
-        response = []
-        for arg in message.args:
-            record = get_votes_record(arg)
-            response.append(f'{record.label} has {record.votes} {"vote" if record.votes == 1 else "votes"}')
-        return '\n'.join(response)
+        if message.command == 'votes':
+            response = []
+            for arg in message.args:
+                record = get_votes_record(arg)
+                response.append(f'{record.label} has {self.__format_score(record.votes)}')
+            return '\n'.join(response)
+        elif message.command in ('upvote', '++') and len(message.args) == 1:
+            return self.__do_upvote(message.username, message.args[0])
+        elif message.command in ('downvote', '--') and len(message.args) == 1:
+            return self.__do_downvote(message.username, message.args[0])
 
     def handle_reaction(self, reaction: ReactionEvent) -> Optional[str]:
-        if reaction.emoji == 'upvote':
-            if reaction.username == reaction.message.username and reaction.added:
-                return f'Nice try, {reaction.username}, you can\'t upvote yourself'
-            elif reaction.added:
-                record = upvote(reaction.message.username)
-                return f'{reaction.username} upvoted {reaction.message.username}! {reaction.message.username} has {record.votes} friskypoints.'
-            else:
-                downvote(reaction.message.username)
-        elif reaction.emoji == 'downvote':
-            if reaction.username == reaction.message.username and reaction.added:
-                record = downvote(reaction.message.username)
-                return f'You CAN however, downvote yourself. You have {record.votes} friskypoints'
-            elif reaction.added:
-                record = downvote(reaction.message.username)
-                return f'{reaction.username} downvoted {reaction.message.username}! {reaction.message.username} has {record.votes} friskypoints.'
-            else:
-                upvote(reaction.message.username)
+        if reaction.added:
+            if reaction.emoji == 'upvote':
+                return self.__do_upvote(reaction.username, reaction.message.username)
+            elif reaction.emoji == 'downvote':
+                return self.__do_downvote(reaction.username, reaction.message.username)
+        else:
+            if reaction.emoji == 'upvote':
+                return self.__do_downvote(reaction.username, reaction.message.username, silent=True)
+            elif reaction.emoji == 'downvote':
+                return self.__do_upvote(reaction.username, reaction.message.username, silent=True)
--- tests/test_votes.py
+++ tests/test_votes.py
@@ -9,6 +9,37 @@ class VoteTestCase(FriskyTestCase):
         record = get_votes_record('user_b')
         self.assertEqual(record.votes, 1)
 
+    def test_downvote_user_has_neg_one_vote(self):
+        self.send_reaction('downvote', 'user_a', 'user_b')
+        record = get_votes_record('user_b')
+        self.assertEqual(record.votes, -1)
+
     def test_vote_query(self):
         reply = self.send_message('?votes user_b')
-        self.assertEqual(reply, 'user_b has 0 votes')
+        self.assertTrue(reply.startswith('user_b has 0'))
+        record = get_votes_record('user_b')
+        self.assertEqual(record.votes, 0)
+
+    def test_upvote_via_cmd(self):
+        self.send_message('?upvote stonks')
+        record = get_votes_record('stonks')
+        self.assertEqual(record.votes, 1)
+
+    def test_upvote_via_alias(self):
+        self.send_message('?++ stonks')
+        self.send_message('?++ stonks')
+        self.send_message('?++ stonks')
+        record = get_votes_record('stonks')
+        self.assertEqual(record.votes, 3)
+
+    def test_downvote_via_cmd(self):
+        self.send_message('?downvote stonks')
+        record = get_votes_record('stonks')
+        self.assertEqual(record.votes, -1)
+
+    def test_downvote_via_alias(self):
+        self.send_message('?-- stonks')
+        self.send_message('?-- stonks')
+        self.send_message('?-- stonks')
+        record = get_votes_record('stonks')
+        self.assertEqual(record.votes, -3)
