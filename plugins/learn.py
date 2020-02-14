8a9e035db72126bffc6399d3dc847ce885639dc6
Jim Carreer <jim.carreer@gmail.com>
2020-02-14 05:26:41 -0500

    Fix formatting / tweak learn help text a bit

--- plugins/learn.py
+++ plugins/learn.py
@@ -11,12 +11,12 @@ class LearnPlugin(FriskyPlugin):
     def help_text(cls) -> Optional[str]:
         return '\n'.join([
             '*Command Usage*',
-            '* `?<label>` - return a random value from `<label>`',
-            '* `?learn <label>` - get a random quote for `<label>`',
-            '* `?learn <label> <value>` - add <value> to <label>',
-            '* `?learn_count - show the counts of learned phrases by label (alias: `?lc`)',
+            ' • `?learn <label> <index>` - get a value for `<label>`, if `<index>` is not sent one is chosen at random',
+            ' • `?<label> <index>` - short hand for the `?learn <label> ...`',
+            ' • `?learn <label> <value>` - add <value> to <label>',
+            ' • `?learn_count - show the counts of learned phrases by label (alias: `?lc`)',
             '*Emoji*',
-            '* Tag a user message with :brain: for frisky to learn a quote form that user'
+            ' • Tag a user message with :brain: for frisky to learn a quote form that user'
         ])
 
     @classmethod
@@ -40,7 +40,7 @@ class LearnPlugin(FriskyPlugin):
 
     @staticmethod
     def learn_count() -> Optional[str]:
-        return '*Counts*\n'+('\n'.join([f'* {lc["label"]}: {lc["total"]}' for lc in get_learned_label_counts()]))
+        return '*Counts*\n'+('\n'.join([f' • {lc["label"]}: {lc["total"]}' for lc in get_learned_label_counts()]))
 
     @staticmethod
     def learn(message: MessageEvent) -> Optional[str]:
