74faea8ab67b448aab29e085db6ad7aa490760d3
Zach Tibbitts <zachtib@gmail.com>
2020-02-14 08:53:43 -0500

    Refactor of the learn plugin, as well as how generic handlers are, well, handled.
    Makes the learn plugin case-insensitive. ie: ?foo == ?Foo

--- frisky/bot.py
+++ frisky/bot.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 class Frisky(object):
     name: str
     prefix: str
-    __loaded_plugins: List[FriskyPlugin]
+    __loaded_plugins: Dict[str, FriskyPlugin]
     __message_handlers: Dict[str, List[FriskyPlugin]]
     __reaction_handlers: Dict[str, List[FriskyPlugin]]
 
@@ -22,7 +22,7 @@ class Frisky(object):
         super().__init__()
         self.name = name
         self.prefix = prefix
-        self.__loaded_plugins = list()
+        self.__loaded_plugins = dict()
         self.__message_handlers = dict()
         self.__reaction_handlers = dict()
         self.ignored_channels = ignored_channels
@@ -36,12 +36,12 @@ class Frisky(object):
                 submodule = importlib.import_module(f'{module}.{name}')
                 for item_name, item in inspect.getmembers(submodule):
                     if inspect.isclass(item) and item is not FriskyPlugin and issubclass(item, FriskyPlugin):
-                        self.__load_plugin_from_class(item)
+                        self.__load_plugin_from_class(name, item)
 
-    def __load_plugin_from_class(self, cls) -> None:
+    def __load_plugin_from_class(self, name, cls) -> None:
         try:
             plugin = cls()
-            self.__loaded_plugins.append(plugin)
+            self.__loaded_plugins[name] = plugin
             for command in cls.register_commands():
                 handlers = self.__message_handlers.get(command, list())
                 handlers.append(plugin)
@@ -54,40 +54,51 @@ class Frisky(object):
             logger.warning(f'Error instantiating plugin {cls}', exc_info=err)
 
     def get_plugins_for_command(self, command: str) -> List[FriskyPlugin]:
-        result = self.__message_handlers.get(command, list())
-        if len(result) == 0:
-            result = self.__message_handlers.get('*', list())
-        return result
+        return self.__message_handlers.get(command, list())
+
+    def get_generic_handlers(self) -> List[FriskyPlugin]:
+        return self.__message_handlers.get('*', list())
 
     def get_plugins_for_reaction(self, reaction: str) -> List[FriskyPlugin]:
         return self.__reaction_handlers.get(reaction, list())
 
+    @staticmethod
+    def convert_message_to_generic(message: MessageEvent) -> MessageEvent:
+        message.args = (message.command,) + message.args
+        message.command = '*'
+        return message
+
     def __show_help_text(self, args: Tuple[str], reply_channel: Callable[[str], bool]) -> None:
         if len(args) == 1:
-            command = args[0]
-            if command == 'help':
+            plugin_name = args[0]
+            if plugin_name == 'help':
                 reply_channel('Usage: `?help` or `?help <plugin_name>`')
                 return
-            help_texts = []
-            for plugin in self.get_plugins_for_command(command):
-                help_texts.append(plugin.help_text())
-            if len(help_texts) > 0:
-                reply_channel('\n'.join(help_texts))
+            plugin = self.__loaded_plugins.get(plugin_name, None)
+            if plugin is None:
+                reply_channel(f'No such plugin: `{plugin_name}`, try `?help` to list installed plugins')
+            elif (help_text := plugin.help_text()) is None:
+                reply_channel(f'Plugin `{plugin_name}` does not provide help text.')
             else:
-                reply_channel(f'No help found for {command}')
+                reply_channel(help_text)
         else:
-            commands = self.__message_handlers.keys()
-            joined_string = ', '.join(commands)
+            plugins = self.__loaded_plugins.keys()
+            joined_string = ', '.join(plugins)
             reply_channel(f'Available plugins: {joined_string}')
 
     def handle_message(self, message: MessageEvent, reply_channel: Callable[[str], bool]) -> None:
         if message.channel_name in self.ignored_channels or message.username == self.name:
             return
         message.command, message.args = self.parse_message_string(message.text)
-        if message.command == 'help':
+        if message.command in ('help', '?'):
             return self.__show_help_text(message.args, reply_channel)
         elif message.command != '':
-            for plugin in self.get_plugins_for_command(message.command):
+            plugins = self.get_plugins_for_command(message.command)
+            if len(plugins) == 0:
+                # Reformat the message as a generic one
+                message = self.convert_message_to_generic(message)
+                plugins = self.get_generic_handlers()
+            for plugin in plugins:
                 reply = plugin.handle_message(message)
                 if reply is not None:
                     reply_channel(reply)
