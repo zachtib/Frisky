8cbd5421988ed9ab633c1918418b6b16644ed500
Zach Tibbitts <zachtib@gmail.com>
2020-02-19 14:53:28 -0500

    fix deprecations

--- tests/test_meme.py
+++ tests/test_meme.py
@@ -11,7 +11,7 @@ meme_list = '''
          {
             "id":"181913649",
             "name":"Drake Hotline Bling",
-            "url":"https:\/\/i.imgflip.com\/30b1gx.jpg",
+            "url":"https://i.imgflip.com/30b1gx.jpg",
             "width":1200,
             "height":1200,
             "box_count":2
@@ -19,7 +19,7 @@ meme_list = '''
          {
             "id":"112126428",
             "name":"Distracted Boyfriend",
-            "url":"https:\/\/i.imgflip.com\/1ur9b0.jpg",
+            "url":"https://i.imgflip.com/1ur9b0.jpg",
             "width":1200,
             "height":800,
             "box_count":3
@@ -27,7 +27,7 @@ meme_list = '''
          {
             "id":"87743020",
             "name":"Two Buttons",
-            "url":"https:\/\/i.imgflip.com\/1g8my4.jpg",
+            "url":"https://i.imgflip.com/1g8my4.jpg",
             "width":600,
             "height":908,
             "box_count":2
