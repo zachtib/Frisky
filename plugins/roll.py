99f7dc4b6dc846ac4bc910b621f02e90b9596615
Zach Tibbitts <zachtib@gmail.com>
2020-03-25 09:35:05 -0400

    better dice

--- plugins/roll.py
+++ plugins/roll.py
@@ -1,31 +1,76 @@
+import re
+from dataclasses import dataclass
 from random import randint
-from typing import Tuple
+from typing import Tuple, List, Optional
 
 from frisky.events import MessageEvent
 from frisky.plugin import FriskyPlugin
 from frisky.responses import FriskyResponse
 
 
+@dataclass
+class DieRoll:
+    count: int
+    die: int
+    modifier: Optional[int] = None
+
+
+regex = re.compile(r'(\d+)?d(\d+)([+\-]\d+)?')
+
+
+def parse_single_die_syntax(expr: str) -> Optional[DieRoll]:
+    m = regex.match(expr)
+    if m is None:
+        return None
+    return DieRoll(
+        count=int(m.group(1) or 1),  # Default to rolling 1 die,
+        die=int(m.group(2)),  # This is the only hard required value
+        modifier=int(m.group(3) or 0)  # Default to zero modifier
+    )
+
+
+def parse_dice_syntax(inputs: List[str]) -> List[DieRoll]:
+    return [parse_single_die_syntax(expr) for expr in inputs]
+
+
+def calculate_roll(roll: DieRoll) -> int:
+    total = 0
+    for i in range(0, roll.count):
+        result = randint(1, roll.die)
+        total += result
+    if roll.modifier:
+        total += roll.modifier
+    return total
+
+
 class RollPlugin(FriskyPlugin):
+
+    def __init__(self) -> None:
+        super().__init__()
 
     @classmethod
     def register_commands(cls) -> Tuple:
         return 'roll',
 
     def handle_message(self, message: MessageEvent) -> FriskyResponse:
-        dice = []
-        for arg in message.args:
-            try:
-                int_arg = int(arg)
-                if int_arg > 1:
-                    dice.append(int_arg)
-            except ValueError:
-                pass
-        if len(dice) == 0:
-            dice = [20]
+        if len(message.args) == 0:
+            # With no inputs, default to rolling 1d20
+            d20 = DieRoll(count=1, die=20)
+            roll = calculate_roll(d20)
+            return f'Rolling 1d20 for {message.username}: {roll}'
+
+        parsed_rolls = parse_dice_syntax(message.args)
         results = []
-        for die in dice:
-            result = randint(1, die)
-            results.append(str(result))
+        errors = []
+        for expr, roll in zip(message.args, parsed_rolls):
+            if roll is None:
+                errors.append(expr)
+                results.append('???')
+            else:
+                results.append(str(calculate_roll(roll)))
         result_string = ', '.join(results)
-        return f'{message.username} rolled {result_string}'
+        errors_string = ', '.join(errors)
+        message = f'{message.username} rolled {result_string}'
+        if errors_string:
+            message += f"... I don't know how to roll {errors_string}"
+        return message
--- tests/test_roll.py
+++ tests/test_roll.py
@@ -8,27 +8,42 @@ class RollTestCase(FriskyTestCase):
     def test_roll_defaults_to_d20(self):
         patcher = mock.patch(target='plugins.roll.randint', new=lambda *a, **k: 10)
         patcher.start()
-        self.assertEqual('dummyuser rolled 10', self.send_message('?roll'))
+        self.assertEqual('Rolling 1d20 for dummyuser: 10', self.send_message('?roll'))
         patcher.stop()
 
     def test_roll_takes_param(self):
         def roll(arg1, arg2):
             self.assertEqual(1, arg1)
             self.assertEqual(6, arg2)
+            return 0
 
         patcher = mock.patch(target='plugins.roll.randint', new=roll)
         patcher.start()
-        self.send_message('?roll 6')
+        self.send_message('?roll 1d6')
         patcher.stop()
 
-    def test_invalid_input_rolls_20(self):
+    def test_roll_considers_modifier(self):
+        patcher = mock.patch(target='plugins.roll.randint', new=lambda *a, **k: 0)
+        patcher.start()
+        result = self.send_message('?roll 1d6+1')
+        patcher.stop()
+        self.assertEqual('dummyuser rolled 1', result)
+
+    def test_roll_considers_negative_modifier(self):
+        patcher = mock.patch(target='plugins.roll.randint', new=lambda *a, **k: 0)
+        patcher.start()
+        result = self.send_message('?roll 1d6-1')
+        patcher.stop()
+        self.assertEqual('dummyuser rolled -1', result)
+
+    def test_invalid_input_shames_user(self):
         patcher = mock.patch(target='plugins.roll.randint', new=lambda *a, **k: 10)
         patcher.start()
-        self.assertEqual('dummyuser rolled 10', self.send_message('?roll potato'))
+        self.assertEqual('dummyuser rolled ???... I don\'t know how to roll potato', self.send_message('?roll potato'))
         patcher.stop()
 
     def test_multiple_inputs(self):
         patcher = mock.patch(target='plugins.roll.randint', new=lambda *a, **k: 10)
         patcher.start()
-        self.assertEqual('dummyuser rolled 10, 10, 10', self.send_message('?roll 20 20 20'))
+        self.assertEqual('dummyuser rolled 10, 10, 10', self.send_message('?roll 1d20 1d20 1d20'))
         patcher.stop()
