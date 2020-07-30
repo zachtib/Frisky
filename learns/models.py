394be32e406aad975d5b9256c767b0b1341a6dcd
Zach Tibbitts <zachtib@gmail.com>
2020-07-30 19:28:53 -0400

    Increase learn length limit to 2000 characters (#80)

--- learns/models.py
+++ learns/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Learn(models.Model):
     label = models.CharField(max_length=50, db_index=True)
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
 
     def __str__(self):
         return f'{self.label}: "{self.content}"'
