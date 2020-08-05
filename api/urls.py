112ae49fdce9dcb401b5cc5fa9c6bbcbeb808add
Zach Tibbitts <zachtib@gmail.com>
2020-08-05 16:10:28 -0400

    missing url

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import random_learn
+from .views import random_learn, get_response
 
 urlpatterns = [
+    path('response/', get_response),
     path('random/', random_learn)
 ]
