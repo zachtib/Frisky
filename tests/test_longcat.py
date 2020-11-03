5f7a09e5ef6b61a921d6df41e4b085dc0e86cd41
Zach Tibbitts <zachtib@gmail.com>
2020-11-03 13:52:06 -0500

    cat (#93)

---
+++ tests/test_longcat.py
@@ -0,0 +1,33 @@
+from frisky.test import FriskyTestCase
+
+
+class LearnTestCase(FriskyTestCase):
+
+    def test_no_arguments_returns_nothing(self):
+        self.assertIsNone(self.send_message('?longcat'))
+
+    def test_two_arguments_returns_nothing(self):
+        self.assertIsNone(self.send_message('?longcat banana pudding'))
+
+    def test_nonint_argument_returns_nothing(self):
+        self.assertIsNone(self.send_message('?longcat pudding'))
+
+    def test_longcat_1(self):
+        actual = self.send_message('?longcat 1')
+        expected = (
+            ':longcat-begin:\n'
+            ':longcat-middle:\n'
+            ':longcat-end:'
+        )
+        self.assertEqual(expected, actual)
+
+    def test_longcat_3(self):
+        actual = self.send_message('?longcat 3')
+        expected = (
+            ':longcat-begin:\n'
+            ':longcat-middle:\n'
+            ':longcat-middle:\n'
+            ':longcat-middle:\n'
+            ':longcat-end:'
+        )
+        self.assertEqual(expected, actual)
