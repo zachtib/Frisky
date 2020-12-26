87ff7834ef4c0c87acf0b209062bf351f7d247fe
Zach Tibbitts <zachtib@gmail.com>
2020-12-26 14:29:40 -0500

    Add leaderboard (#98)

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -2,49 +2,49 @@ from datetime import date
 from unittest.mock import patch
 
 from frisky.test import FriskyTestCase
-from votes.queries import get_votes_record
+from votes.models import Vote
 
 
 class VoteTestCase(FriskyTestCase):
 
     def test_upvote_user_has_one_vote(self):
         self.send_reaction('upvote', 'user_a', 'user_b')
-        record = get_votes_record('user_b')
+        record = Vote.objects.get_record('user_b')
         self.assertEqual(record.votes, 1)
 
     def test_downvote_user_has_neg_one_vote(self):
         self.send_reaction('downvote', 'user_a', 'user_b')
-        record = get_votes_record('user_b')
+        record = Vote.objects.get_record('user_b')
         self.assertEqual(record.votes, -1)
 
     def test_vote_query(self):
         reply = self.send_message('?votes user_b')
         self.assertTrue(reply.startswith('user_b has 0'))
-        record = get_votes_record('user_b')
+        record = Vote.objects.get_record('user_b')
         self.assertEqual(record.votes, 0)
 
     def test_upvote_via_cmd(self):
         self.send_message('?upvote stonks')
-        record = get_votes_record('stonks')
+        record = Vote.objects.get_record('stonks')
         self.assertEqual(record.votes, 1)
 
     def test_upvote_via_alias(self):
         self.send_message('?++ stonks')
         self.send_message('?++ stonks')
         self.send_message('?++ stonks')
-        record = get_votes_record('stonks')
+        record = Vote.objects.get_record('stonks')
         self.assertEqual(record.votes, 3)
 
     def test_downvote_via_cmd(self):
         self.send_message('?downvote stonks')
-        record = get_votes_record('stonks')
+        record = Vote.objects.get_record('stonks')
         self.assertEqual(record.votes, -1)
 
     def test_downvote_via_alias(self):
         self.send_message('?-- stonks')
         self.send_message('?-- stonks')
         self.send_message('?-- stonks')
-        record = get_votes_record('stonks')
+        record = Vote.objects.get_record('stonks')
         self.assertEqual(record.votes, -3)
 
     def test_festive_messages(self):
@@ -67,7 +67,6 @@ class VoteTestCase(FriskyTestCase):
             mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
             response = self.send_message('?upvote user')
             self.assertEqual('dummyuser upvoted user! user has 1 friskypoint', response)
-
 
     def test_festive_messages_pl(self):
         self.send_message('?upvote user')
@@ -92,3 +91,12 @@ class VoteTestCase(FriskyTestCase):
             mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
             response = self.send_message('?upvote user')
             self.assertEqual('dummyuser upvoted user! user has 2 friskypoints', response)
+
+    def test_leaderboard(self):
+        Vote.objects.create(label='foo', votes=3)
+        Vote.objects.create(label='bar', votes=1)
+        Vote.objects.create(label='foobar', votes=4)
+        response = self.send_message('?leaderboard')
+        self.assertEqual('foobar: 4\n'
+                         'foo: 3\n'
+                         'bar: 1', response)
