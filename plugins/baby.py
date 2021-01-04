f762c6948458ab480b2ddb966c4a018daaab8e81
Zach Tibbitts <zachtib@gmail.com>
2021-01-04 17:58:33 -0500

    alias

--- plugins/baby.py
+++ plugins/baby.py
@@ -3,5 +3,8 @@ from frisky.plugin import FriskyApiPlugin
 
 class BabyTrackerPlugin(FriskyApiPlugin):
     commands = ['baby']
+    command_aliases = {
+        'hasasiahadthebabyyet': 'baby',
+    }
     url = 'https://hasasiahadthebabyyet.herokuapp.com/api/'
     json_property = 'display'
